ce5a72d81addbb6737a94be9efa9adb54845250d
L0G1C06 <justgithub70@gmail.com>
2024-08-06 11:59:54 -0300

    feat: take picture page implemented

--- scripts/app.py
+++ scripts/app.py
@@ -1,5 +1,8 @@
 import flet as ft
 from flet import *
+import os
+import cv2
+import time
 
 def create_base_container(content):
     return ft.Container(
@@ -24,7 +27,7 @@ def main(page: ft.Page):
         username = username_login.value
         password = password_login.value
         # Aqui você pode adicionar a lógica para verificar o login
-        if username == "admin" and password == "admin":
+        if username == "" and password == "":
             page.controls.clear()
             page.add(create_base_container(home_page(username)))
         else:
@@ -59,11 +62,73 @@ def main(page: ft.Page):
             controls=[
                 ft.Text(f"Olá {username}!", size=24, weight="bold", color='white'),
                 # Adicione outros controles para a página inicial aqui
+                ft.ElevatedButton(text="Tirar foto", on_click=lambda e: go_to_camera_page(None)),
                 ft.ElevatedButton(text="Logout", on_click=lambda e: switch_to_login(None))
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER
         )
+
+    def go_to_camera_page(e):
+        page.controls.clear()
+        page.add(create_base_container(camera_page()))
+        page.update()
+    
+    def camera_page():
+        myimage = ft.Image(
+            src=False,
+            width=300,
+            height=300,
+            fit='cover'
+        )
+
+        def removeallyouphoto():
+            folder_path = "images/"
+            files = os.listdir(folder_path)
+            for file in files:
+                file_path = os.path.join(folder_path, file)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                    print(f"File successfully removed: {file_path}")
+            page.update()
+
+        def takemepicture(e):
+            removeallyouphoto()
+            cap = cv2.VideoCapture(0)
+            cv2.namedWindow("Webcam", cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("Webcam", 400, 600)
+            timestamp = str(int(time.time()))
+            myfileface = f"myPhoto_{timestamp}.jpg"
+            try:
+                while True:
+                    ret, frame = cap.read()
+                    cv2.imshow("Webcam", frame)
+                    page.update()
+                    key = cv2.waitKey(1)
+                    if key == ord("q"):
+                        break
+                    elif key == ord("s"):
+                        cv2.imwrite(f"images/{myfileface}", frame)
+                        cv2.putText(frame, "Capture successful!", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                        cv2.imshow("Webcam", frame)
+                        cv2.waitKey(3000)
+                        folder_path = "images/"
+                        myimage.src = folder_path + myfileface
+                        page.update()
+                        break
+                cap.release()
+                cv2.destroyAllWindows()
+                page.update()
+            except Exception as e:
+                print(e)
+                print("Error during capture")
+
+        return ft.Column([
+            ft.Text("Webcam Capture Your Face", size=30, weight="bold"),
+            ft.ElevatedButton("Take My Face", bgcolor="blue", color="white", on_click=takemepicture),
+            myimage,
+            ft.ElevatedButton(text="Voltar para Home", on_click=lambda e: page.controls.clear() or page.add(create_base_container(home_page(""))))
+        ])
 
     # Formulário de Login
     username_login = ft.TextField(label="Usuário", width=300)
@@ -105,4 +170,4 @@ def main(page: ft.Page):
     page.add(create_base_container(login_form))
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, assets_dir='images')
